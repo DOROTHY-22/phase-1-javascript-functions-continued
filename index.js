022884f057915313975647b000d6d3cd45534c19
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-06 12:48:51 +0300

    Finish up my assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function saturdayFun(activity = "roller-skate") {
+/*function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`;
   }
   function mondayWork(activity = "go to the office") {
@@ -9,8 +9,18 @@ function saturdayFun(activity = "roller-skate") {
     return function(adjective) {
       return `You are ${wrapper}${adjective}${wrapper}!`;
     };
+  }*/
+ function saturdayFun (activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;
+ }
+ function mondayWork (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+ }  
+function wrapAdjective(wrapper = "*"){
+  return function(appreciation = "special"){
+    return `You are ${wrapper}${appreciation}${wrapper}!`;
   }
-    
+}
 
 
 
